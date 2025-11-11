86e617e8263bae5f90cb457122a598fc1becf9da
HiroUzu1 <uzumahirokoshi4@gmail.com>
2025-11-11 16:00:33 -0500

    CH

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/authController');
-const auth = require('../middleware/auth');
+const auth = require('../middleware/authMiddleware');
 
 router.post('/register', ctrl.register);
 router.post('/login', ctrl.login);
